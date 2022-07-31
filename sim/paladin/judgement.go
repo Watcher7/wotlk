afa57382a5cd3cf743728340af5266b3473802cc
Watcher7 <Watcher7@users.noreply.github.com>
2022-07-31 02:35:16 -0500

    Dummy change, remove comment, rename variable

--- sim/paladin/judgement.go
+++ sim/paladin/judgement.go
@@ -17,7 +17,7 @@ func (paladin *Paladin) canJudgement(sim *core.Simulation) bool {
 func (paladin *Paladin) registerJudgementOfWisdomSpell(cdTimer *core.Timer) {
 	// paladin.JudgementOfLightAura = core.JudgementOfLightAura(paladin.CurrentTarget)
 
-	baseCost := paladin.BaseMana * 0.05
+	baseCost1 := paladin.BaseMana * 0.05
 
 	paladin.JudgementOfWisdom = paladin.RegisterSpell(core.SpellConfig{
 		ActionID:    core.ActionID{SpellID: 53408},
@@ -25,11 +25,11 @@ func (paladin *Paladin) registerJudgementOfWisdomSpell(cdTimer *core.Timer) {
 		Flags:       SpellFlagPrimaryJudgement,
 
 		ResourceType: stats.Mana,
-		BaseCost:     baseCost,
+		BaseCost:     baseCost1,
 
 		Cast: core.CastConfig{
 			DefaultCast: core.Cast{
-				Cost: baseCost * (1 - 0.02*float64(paladin.Talents.Benediction)),
+				Cost: baseCost1 * (1 - 0.02*float64(paladin.Talents.Benediction)),
 				GCD:  core.GCDDefault,
 			},
 			IgnoreHaste: true,
@@ -48,38 +48,8 @@ func (paladin *Paladin) registerJudgementOfWisdomSpell(cdTimer *core.Timer) {
 	})
 }
 
-// Defines judgement refresh behavior from attacks
-// Returns extra mana if a different pally applied Judgement of Wisdom
-// func (paladin *Paladin) setupJudgementRefresh() {
-// 	const mana = 74 / 2
-// 	paladin.RegisterAura(core.Aura{
-// 		Label:    "Refresh Judgement",
-// 		Duration: core.NeverExpires,
-// 		OnReset: func(aura *core.Aura, sim *core.Simulation) {
-// 			aura.Activate(sim)
-// 		},
-// 		OnSpellHitDealt: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, spellEffect *core.SpellEffect) {
-// 			if spellEffect.Landed() && spellEffect.ProcMask.Matches(core.ProcMaskMeleeWhiteHit) {
-// 				if paladin.CurrentJudgement != nil && paladin.CurrentJudgement.IsActive() {
-// 					// Refresh the judgement
-// 					paladin.CurrentJudgement.Refresh(sim)
-
-// 					// Check if current judgement is not JoW and also that JoW is on the target
-// 					if paladin.CurrentJudgement != paladin.JudgementOfWisdomAura && paladin.JudgementOfWisdomAura.IsActive() {
-// 						// Just trigger a second JoW
-// 						if paladin.JowManaMetrics == nil {
-// 							paladin.JowManaMetrics = paladin.NewManaMetrics(core.ActionID{SpellID: 27164})
-// 						}
-// 						paladin.AddMana(sim, mana, paladin.JowManaMetrics, false)
-// 					}
-// 				}
-// 			}
-// 		},
-// 	})
-// }
-
 func (paladin *Paladin) registerJudgements() {
 	// Shared CD for all judgements.
-	cdTimer := paladin.NewTimer()
-	paladin.registerJudgementOfWisdomSpell(cdTimer)
+	cdTimer1 := paladin.NewTimer()
+	paladin.registerJudgementOfWisdomSpell(cdTimer1)
 }
